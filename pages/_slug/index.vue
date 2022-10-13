1edf6c6458cdd0d403938fc69052fb3b11ae8d06
sankaku_sato <310rin@3kaku.co.jp>
2022-10-13 14:12:16 +0900

    sec commit

---
+++ pages/_slug/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <main class="main">
+    <h1 class="title">{{ title }}</h1>
+    <p class="publishedAt">{{ publishedAt }}</p>
+    <div class="post" v-html="body"></div>
+  </main>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'MicrocmsNuxtJamstackBlogIndex',
+
+  async asyncData({ params }) {
+    const { data } = await axios.get(
+      `https://310blog.microcms.io/api/v1/blog/${params.slug}`,
+      {
+        headers: { 'X-MICROCMS-API-KEY': 'f91abb870a2d48f8aa7a0e7419f36d2d9765' }
+      }
+    )
+    return data
+  }
+
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <ul>
+    <li v-for="content in contents" :key="content.id">
+      <nuxt-link :to="`/${content.id}`">
+        {{ content.title }}
+      </nuxt-link>
+      
+    </li>
+  </ul>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'IndexPage',
+  
+  async asyncData() {
+    const { data } = await axios.get(
+      // your-service-id部分は自分のサービスidに置き換えてください
+      'https://310blog.microcms.io/api/v1/blog',
+      {
+        // your-api-key部分は自分のapi-keyに置き換えてください
+        headers: { 'X-MICROCMS-API-KEY': 'f91abb870a2d48f8aa7a0e7419f36d2d9765' }
+      }
+    )
+    console.log(data);
+    return data
+  }
+}
+</script>
